b9f5dccb4bc03271774c3df83ee2f184ccc0b994
ahmed atef <99121164+ahmedatef9693@users.noreply.github.com>
2022-11-02 23:46:55 +0200

    Create index.js

---
+++ Dom/index.js
@@ -0,0 +1,13 @@
+
+
+function showElements(){
+//    let docClass = document.getElementsByClassName("divs");
+//     console.log(docClass[0]);
+//     let docId = document.getElementById("divtwo");
+//     console.log(docId);
+
+    let elements = document.getElementsByTagName("div");
+    console.log(elements[0]);
+
+
+}
